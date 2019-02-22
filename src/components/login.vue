3ce703c741d6457af9bd721ee9f7a1dc84e5bb02
DESKTOP-G4J2BOL\EDZ <949246160@qq.com>
2019-02-22 19:07:21 +0800

    配置axios拦截器

--- src/components/login.vue
+++ src/components/login.vue
@@ -19,7 +19,7 @@
 				<el-button @click="login" class='login_btn' type="primary">登录</el-button>
 				
 
-				<!-- <div id="myChart" :style="{width: '300px', height: '300px'}"></div> -->
+				<div id="myChart" :style="{width: '300px', height: '300px'}"></div>
 
 
 			</div>
@@ -44,31 +44,31 @@
 			}
 		},
 		mounted(){
-			// this.drawLine();
+			this.drawLine();
 		},
 		methods: {
 			login:function() {
 				console.log(this.phone)
 				console.log(this.code)
 			},
-// 			drawLine() {
-// 			  // 基于准备好的dom，初始化echarts实例
-// 			  let myChart = echarts.init(document.getElementById('myChart'))
-// 			  // 绘制图表
-// 			  myChart.setOption({
-// 				title: { text: 'ECharts 入门示例' },
-// 				tooltip: {},
-// 				xAxis: {
-// 				  data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
-// 				},
-// 				yAxis: {},
-// 				series: [{
-// 				  name: '销量',
-// 				  type: 'bar',
-// 				  data: [5, 20, 36, 10, 10, 20]
-// 				}]
-// 			  });
-// 			}
+			drawLine() {
+			  // 基于准备好的dom，初始化echarts实例
+			  let myChart = this.$echarts.init(document.getElementById('myChart'))
+			  // 绘制图表
+			  myChart.setOption({
+				title: { text: 'ECharts 入门示例' },
+				tooltip: {},
+				xAxis: {
+				  data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+				},
+				yAxis: {},
+				series: [{
+				  name: '销量',
+				  type: 'bar',
+				  data: [5, 20, 36, 10, 10, 20]
+				}]
+			  });
+			}
 		},
 	}
 </script>
