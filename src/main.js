ddca1926f6ae1abbb57c7ee94ff05c79b557a5b1
DESKTOP-G4J2BOL\EDZ <949246160@qq.com>
2019-03-12 11:47:23 +0800

    axios拦截器 状态码完成

--- src/main.js
+++ src/main.js
@@ -38,6 +38,12 @@ axios.defaults.transformRequest = function (data) {
 	
 }
 
+let vm = new Vue({
+  el: '#app',
+  router,
+})
+
+
 // 添加请求拦截器
 axios.interceptors.request.use(function (config) {
   // 在发送请求之前做些什么
@@ -45,26 +51,43 @@ axios.interceptors.request.use(function (config) {
 	return config;
 }, function (error) {
   // 对请求错误做些什么
+	Vue.prototype.$alert.error('服务器错误，请重试')
 	return Promise.reject(error);
 });
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
 	console.log(response)
-	Vue.prototype.$alert.error('服务器错误，请重试')
 	if(response.status == 200){
-		// return response;
+		if(!response.data.code){
+			return response;
+		}
+		
+		if(response.data.code == 200){
+			
+		}
+		else if(response.data.code == 401){
+			Vue.prototype.$alert.error('登录超时，请重新登录')
+			setTimeout(()=>{
+				window.localStorage.clear()
+				vm.$router.push({path:'/'})
+			},1500)
+			
+		}
+		else if(response.data.code == 500){
+			Vue.prototype.$alert.error('服务器错误，请重试')
+		}
+		else{
+			Vue.prototype.$alert.error(response.data.msg)
+		}
+		return response;
 	}
 	else{
-		
+		Vue.prototype.$alert.error('服务器错误，请重试')
 	}
-	if(response.data.code == 200){
-		console.log(1)
-	}
-  // 对响应数据做点什么
-	
+
  }, function (error) {
-	 // console.log(111)
   // 对响应错误做点什么
+	Vue.prototype.$alert.error('服务器错误，请重试')
 	return Promise.reject(error);
 });
 
